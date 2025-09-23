8ce75acd0aaea4a8bd340fe6d64d899cc94e3f99
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-22 22:44:23 -0300

    Update Exercicio5.java
    Exercicio 5  - Calcular a area de um quadrado

--- exercicios/grupo1/Exercicio5.java
+++ exercicios/grupo1/Exercicio5.java
@@ -2,3 +2,20 @@
 Calcule a área de um quadrado.  
 Como calcular: Area = lado * lado 
 *******************************************************************************/
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int lado, area;
+        
+        System.out.print("Para calcular  a area de um quadrado, informe o valor: ");
+        lado = scanner.nextInt();
+        area = lado * lado;
+
+        System.out.printf("A área do quadro é %d%n", area);
+        scanner.close();
+    }
+}
