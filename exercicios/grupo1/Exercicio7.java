8ea907036343ec5b1375472649f25c6c798087e9
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-22 23:15:09 -0300

    Update Exercicio7.java
    Exercicio 7 - calcular area retangulo

--- exercicios/grupo1/Exercicio7.java
+++ exercicios/grupo1/Exercicio7.java
@@ -1,3 +1,22 @@
 /******************************************************************************
 Calcule a área de um retângulo. Area = comprimento * altura
 *******************************************************************************/
+
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double area, comprimento, altura;
+        System.out.println("Para calcular a área de um Retangulo, informe o comprimento e altura: ");
+        System.out.print("comprimento: ");
+        comprimento = scanner.nextDouble();
+        System.out.print("altura: ");
+        altura = scanner.nextDouble();
+        area = comprimento * altura;
+
+        System.out.printf("A área do Retangulo é %.2f%n", area);
+        scanner.close();
+    }
+}
