f8e03b17a2cbbfcec6e7a575480027dde366ced6
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-22 21:03:30 -0300

    Update Exercicio1.java
    Exercicio1: escreva o nome do aluno

--- exercicios/grupo1/Exercicio1.java
+++ exercicios/grupo1/Exercicio1.java
@@ -1,6 +1,7 @@
-package exercicios.grupo1;
+// Escrever na tela Ola Mundo 
 
 public class Exercicio1 {
+    
     public static void main(String[] args) {
         System.out.println("Ola Mundo!");
     }
