4109a0d62afe67cc00f2abf0e7e8bce436a1f12c
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-22 22:02:26 -0300

    Update Exercicio3.java
    Exercicio3 - Calcular média do aluno

--- exercicios/grupo1/Exercicio3.java
+++ exercicios/grupo1/Exercicio3.java
@@ -1,1 +1,30 @@
+/******************************************************************************
+Perguntar o nome de um aluno, pergunte as 4 notas e calcule a média aritmética(média 
+simples), exibindo no final. 
+*******************************************************************************/
 
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe o nome do aluno: ");
+        String aluno = scanner.nextLine();
+        
+        double nota1, nota2, nota3, nota4, media;
+        
+        System.out.print("Digite a primeira: ");
+        nota1 = scanner.nextDouble();
+        System.out.print("Digite a segundo: ");
+        nota2 = scanner.nextDouble();
+        System.out.print("Digite a terceira: ");
+        nota3 = scanner.nextDouble();
+        System.out.print("Digite a quarta: ");
+        nota4 = scanner.nextDouble();
+        media = (nota1+nota2+nota3+nota4)/4;
+
+        System.out.printf("%s a sua média é: %.2f%n", aluno, media);
+        scanner.close();
+    }
+}
