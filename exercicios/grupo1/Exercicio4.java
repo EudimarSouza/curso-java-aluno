0c4eb02654442842a4a12c322ce4af03106b08c4
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-22 22:30:14 -0300

    Update Exercicio4.java
    Exercicio4 - Calcular o quadrado de um numero

--- exercicios/grupo1/Exercicio4.java
+++ exercicios/grupo1/Exercicio4.java
@@ -2,3 +2,19 @@
 Calcule o quadrado de um número.  
 Pergunte o número, calcule e exiba no final o resultado. 
 *******************************************************************************/
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int quadrado, resultado;
+        System.out.print("Para calcular o quadrado, informe o valor: ");
+        quadrado = scanner.nextInt();
+        resultado = quadrado * quadrado;
+
+        System.out.printf("O valor do quadro: %d%n", resultado);
+        scanner.close();
+    }
+}
