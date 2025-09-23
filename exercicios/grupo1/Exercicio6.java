01f14a7916a8b169682f8426b98dfacb2ea34bff
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-22 22:55:46 -0300

    Update Exercicio6.java
    Exercicio 6 - Calcula a area do Circulo

--- exercicios/grupo1/Exercicio6.java
+++ exercicios/grupo1/Exercicio6.java
@@ -2,3 +2,20 @@
 Calcule a área de um círculo com base no raio informado.  
 Como calcular: A = PI . R ao quadrado 
 *******************************************************************************/
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double a, r;
+        final double pi = 3.141592654;
+        System.out.print("Para calcular a área de um circulo, informe o Raio: ");
+        r = scanner.nextDouble();
+        a = pi * r * r;
+
+        System.out.printf("A área do Circulo é %.2f%n", a);
+        scanner.close();
+    }
+}
