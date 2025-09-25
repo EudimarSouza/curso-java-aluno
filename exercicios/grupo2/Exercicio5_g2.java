a08052ca2fa98ff6df3b6b6f2468b92d541552d8
Eudimar Souza <eudimar84@hotmail.com>
2025-09-24 22:50:30 -0300

    Exercici 5

--- exercicios/grupo2/Exercicio5_g2.java
+++ exercicios/grupo2/Exercicio5_g2.java
@@ -1,5 +1,27 @@
+/*Pergunte um número. Diga se esse número é positivo, negativo ou zero. */
+
 package exercicios.grupo2;
 
-public interface Exercicio5_g2 {
+import java.util.Scanner;
+
+public class Exercicio5_g2 {
+
+    int numero;
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe um numero: ");
+        int numero = scanner.nextInt();
+        if(numero < 0){
+            System.out.print(" O numero " + numero + " é Negativo");
+        }
+        else if(numero >  0){
+            System.out.print(" O numero " + numero + " é positivo");
+        }
+        else{
+            System.out.println("O numero informado é 0(Zero)");
+        }
+        
+        scanner.close();
+    }
 
 }
