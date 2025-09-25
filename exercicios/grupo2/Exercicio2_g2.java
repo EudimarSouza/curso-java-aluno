cd7654ad4f79dd1c6828d005cebc7a0a45fb6996
Eudimar Souza <eudimar84@hotmail.com>
2025-09-24 22:05:33 -0300

    Alteracao Exercicio 2 g2

--- exercicios/grupo2/Exercicio2_g2.java
+++ exercicios/grupo2/Exercicio2_g2.java
@@ -1,3 +1,11 @@
+/*
+--------------------------------------------------------------------------
+Pergunte um número. Diga se este numero é primo ou não, escrevendo na tela. 
+-------------------------------------------------------------------------- 
+
+*/
+
+
 package exercicios.grupo2;
 
 public class Exercicio2_g2{
