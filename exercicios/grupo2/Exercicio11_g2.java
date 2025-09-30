411ed22e0c2a14844bc08b7dc05c3e508f43de5b
Eudimar Souza <eudimar84@hotmail.com>
2025-09-30 19:41:44 -0300

    Eercicios 11 - Cores

--- exercicios/grupo2/Exercicio11_g2.java
+++ exercicios/grupo2/Exercicio11_g2.java
@@ -4,6 +4,32 @@
 
 package exercicios.grupo2;
 
-public class Exercicio11_g2 {
+import java.util.Scanner;
 
+public class Exercicio11_g2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        
+        String letter;
+
+        System.out.println("Digite as Letras V, A ou B para saber a cor relacianada: ");
+        letter = scanner.next();
+
+        switch (letter) {
+            case "v":
+                System.err.printf("V letra %s é correspondente a cor Verde ", letter);
+                break;
+            case "a":
+                System.err.printf("A letra %s é correspondente a cor Azul ", letter);
+                break;
+            case "b":
+                System.err.printf("B letra %s é correspondente a cor Branco ", letter);
+                break;
+            default:
+            System.err.printf("o numero %f é invalido", letter);      
+
+        scanner.close();
+        }
+
+    }
 }
