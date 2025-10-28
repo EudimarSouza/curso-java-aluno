3471e0627947a988a8cf58055599f8065156274d
Eudimar Souza <eudimar84@hotmail.com>
2025-10-27 21:40:04 -0300

    inclusao de comentário

--- exercicios/grupo3/Exercicio14_g3.java
+++ exercicios/grupo3/Exercicio14_g3.java
@@ -15,12 +15,13 @@ public class Exercicio14_g3 {
         double somaNum = 0;
         double media = 0; 
 
+        // for, vai repetir até que qdtNum seja menor ou igual a 5, quando encerrar vai cacular media linha 24
         for(int qdtNum = 1; qdtNum <= 5 ; qdtNum ++){
-            System.out.println("Numemro " + qdtNum); //imprime pergunta do numero 
-            double num = scanner.nextDouble();
-            somaNum += num;
+            System.out.println("Numemro " + qdtNum); //imprime pergunta do numero, e usa o qdtNum para enumerar as vezes 
+            double num = scanner.nextDouble(); //usuario digira o numero
+            somaNum += num; //num é guardado e somado na varialvel somaNum
         }
-        media = somaNum /5;
+        media = somaNum /5; //calcular media
 
         System.out.printf("A média é de %.2f", media);
 
