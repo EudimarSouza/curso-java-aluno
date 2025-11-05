5f216bb8139b53b9ffd05a1208e83daca59b8de2
Eudimar Souza <eudimar84@hotmail.com>
2025-11-04 22:36:33 -0300

    Exercicio 3 - update

--- exercicios/grupo3/Exercicio01_g3.java
+++ exercicios/grupo3/Exercicio01_g3.java
@@ -4,6 +4,30 @@ Pergunte o número, faça o loop e exiba o resultado no final. Exemplo: Fatorial
 
 package exercicios.grupo3;
 
+import java.util.Scanner;
+
 public class Exercicio01_g3 {
+    public static void main (String[] args){
+        // Variaveis
+        int fatorial = 1;
+        int numero = 0;
+        
+
+        //Imprime na tela 
+        System.out.println("Informe o numero para calculo do fatorial:" );
+        // Scanner Classe que realiza leitura de entrada de dados
+        Scanner scanner = new Scanner(System.in);
+        numero = scanner.nextInt();       
+
+        for(int contador = numero; contador >=1; contador--){
+            fatorial = fatorial *contador;
+            
+
+        }
+        System.out.println(fatorial);
+
+        scanner.close();
+
+    }
 
 }
