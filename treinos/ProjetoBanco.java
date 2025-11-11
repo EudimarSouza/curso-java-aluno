c8c83e2572c7698e4f509bf5d8f27f5f85a8747d
Eudimar Souza <eudimar84@hotmail.com>
2025-11-10 22:16:58 -0300

    Update - Opcao 1: Cadastro

--- treinos/ProjetoBanco.java
+++ treinos/ProjetoBanco.java
@@ -4,28 +4,37 @@ import java.util.Scanner;
 
 public class ProjetoBanco {
     public static void main(String[] args){
-        Scanner menu = new Scanner(System.in);
-        int opcao;    
-        while (true) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+        String nomeCompleto, cpf;
 
+        while (true) {
+            
             System.out.print("##-----Banco Gringotes-----##\n\n");
             System.out.print("|-----------------------------|\n");
             System.out.print("| Opção 1 - Cadastro Cliente  |\n");
             System.out.print("| Opção 2 - Login             |\n");
             System.out.print("| Opção 3 - Sair              |\n");
             System.out.print("|-----------------------------|\n");
-            System.out.print("Digite uma opção: ");  
-        
-            opcao = menu.nextInt();
-            if(opcao == 3) {
-                System.out.println("Processo encerrado");
-                menu.close();
+            System.out.print("Digite uma opção: ");            
+    
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            if(opcao == 3){
+                System.out.println("Tchau");
+                break;
             }
 
             switch (opcao) {
-                case 1:
-                    System.out.println("Cadastrar");                    
+                case 1: //Opcao 1 cadastra o cliente
+                    System.out.print("Nome Completo: "); 
+                    nomeCompleto = scanner.nextLine();
+                    System.out.print("CPF: ");
+                    cpf = scanner.nextLine();
+                    System.out.printf("Cliente Cadastrado \nNome: %s | CPF: %s \n\n", nomeCompleto, cpf);             
                     break;
+
                 case 2:
                     System.out.println("Login Cliente");                    
                     break;
@@ -33,11 +42,16 @@ public class ProjetoBanco {
                 default:
                     System.out.println("Ivalida");
                     break;
-            }
-           
+            }            
+
         }
+
+        scanner.close();
     
         
     }
 
+
 }
+
+
